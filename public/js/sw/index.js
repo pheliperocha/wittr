75a946aa7660cb2ab305ef52f222dc401d7be522
Phelipe Rocha <phelipeafonso@gmail.com>
2017-05-01 17:32:10 -0300

    Hijacking JPG

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,8 @@
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        new Response('<h1 class="a-winner-is-me">PhelipeRocha</h1>', {
-            headers: {'Content-Type': 'text/html; charset=utf-8'}
-        })
-    );
+
+    if (event.request.url.endsWith('.jpg')) {
+        event.respondWith(
+            fetch('/imgs/dr-evil.gif')
+        );
+    }
 });
